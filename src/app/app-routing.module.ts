172b8aece18eecc4fdb9003f4cc446b2ae5e7d9c
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-11-02 22:45:02 +0100

    feat: crypto-chips-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CryptoChipsComponent } from './components/crypto-chips/crypto-chips.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
 import { CategoryListComponentModule } from './components/category-list/category-list.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
+import { CryptoChipsComponentModule } from './components/crypto-chips/crypto-chips.component-module';
+import { CryptoServiceModule } from './services/crypto.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'products', component: ProductListComponent }, { path: 'categories', component: CategoryListComponent }]), ProductListComponentModule, ProductsServiceModule, CategoryListComponentModule, CategoriesServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'products', component: ProductListComponent }, { path: 'categories', component: CategoryListComponent }, { path: 'crypto', component: CryptoChipsComponent }]), ProductListComponentModule, ProductsServiceModule, CategoryListComponentModule, CategoriesServiceModule, CryptoChipsComponentModule, CryptoServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
