17aac380eba710dcf16a4721e5379310ffaa0c58
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-12-21 14:56:09 +0100

    categories with loading and  image in empty state + public holidays fix

--- src/app/components/category-checkbox-list/category-checkbox-list.component-module.ts
+++ src/app/components/category-checkbox-list/category-checkbox-list.component-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CategoryCheckboxListComponent } from './category-checkbox-list.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
-  imports: [MatCardModule, MatCheckboxModule, CommonModule],
+    imports: [MatCardModule, MatCheckboxModule, CommonModule, MatProgressSpinnerModule],
   declarations: [CategoryCheckboxListComponent],
   providers: [],
   exports: [CategoryCheckboxListComponent]
