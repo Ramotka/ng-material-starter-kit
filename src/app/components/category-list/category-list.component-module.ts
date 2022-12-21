2fed212527befad15030406d2036fc6aac7f1983
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-12-21 14:12:13 +0100

    categories with spinner and empty state

--- src/app/components/category-list/category-list.component-module.ts
+++ src/app/components/category-list/category-list.component-module.ts
@@ -4,9 +4,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatListModule } from '@angular/material/list';
 import { CategoryListComponent } from './category-list.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
-  imports: [MatCardModule, CommonModule, MatCheckboxModule, MatListModule],
+  imports: [MatCardModule, CommonModule, MatCheckboxModule, MatListModule, MatProgressSpinnerModule],
   declarations: [CategoryListComponent],
   providers: [],
   exports: [CategoryListComponent]
